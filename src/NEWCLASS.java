15f2550b1d7c8c06d966f17a46551f97ba1f2dbe
bpc <matinmoradi110@gmail.com>
2020-03-15 18:53:40 +0330

    hi

--- src/NEWCLASS.java
+++ src/NEWCLASS.java
@@ -3,6 +3,7 @@
 public class NEWCLASS {
     public static void main(String[] args) {
         System.out.println("no");
+        System.out.println("nooooooo");
         System.out.println("YES");
     }
 }
